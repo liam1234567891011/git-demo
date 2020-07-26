95a1090021cce60561c17f7a0b74e12e58c29655
liam1234567891011 <royzhu666@gmail.com>
2020-07-25 23:36:30 -0400

    fix bug in issue1

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,12 +1,17 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Autowired
+    HelloService service;
+
     @GetMapping("/tt")
     public String greetings(){
-        return "hello world";
+        return service.greetings();
     }
 }
---
+++ src/main/java/com/example/demo/service/HelloService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+public class HelloService {
+
+    public String greetings() {
+        return "hello world";
+    }
+}
